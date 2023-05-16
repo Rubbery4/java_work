cb9a7618102159c9485e43838bd20c908d5be33a
Rubber <wh85424@gmail.com>
2023-05-16 15:12:09 +0900

    MemberDao 클래스 작업 마무리

--- Step17_JDBC/src/test/main/MainClass12.java
+++ Step17_JDBC/src/test/main/MainClass12.java
@@ -8,17 +8,14 @@ public class MainClass12 {
 		// 추가할 회원의 정보
 		String name = "덩어리";
 		String addr = "목동";
-		int num = 6;
+		
 		// 추가할 회원의 정보를 MemberDto 객체에 담는다.
 		MemberDto dto = new MemberDto();
 		dto.setName(name);
 		dto.setAddr(addr);
-		dto.setNum(num);
-		
-		// DB에 저장하기, 삭제, 수정
-//		new MemberDao().insert(dto);
-//		new MemberDao().delete(dto);
-//		new MemberDao().update(dto);
+		 
+		// DB에 저장하기
+		new MemberDao().insert(dto);
 		
 	}
 }
--- Step17_JDBC/src/test/main/MainClass15.java
+++ Step17_JDBC/src/test/main/MainClass15.java
@@ -7,11 +7,12 @@ public class MainClass15 {
 		// 삭제할 회원의 정보
 		int num = 2;
 		
-		// 위의 정보를 이용해서 회원정보를 삭제하고 성공이면 "삭제 했습니다" 가 출력 되도록 해 보세요
+//		위의 정보를 이용해서 회원정보를 삭제하고 성공이면 "삭제 했습니다" 가 출력 되도록 해 보세요
 		MemberDao dao = new MemberDao();
 		boolean daoDelete = dao.delete(num);
 		if (daoDelete) {
 			System.out.println("삭제 했습니다.");
 		}
+
 	}
 }
